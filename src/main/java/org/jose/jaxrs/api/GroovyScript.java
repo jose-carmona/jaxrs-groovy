1d2092df8c73204471b1028adc03676f6bc04841
Jose Carmona <j.carmona.n@gmail.com>
2018-08-02 23:50:18 +0200

    groovy funcionando

--- src/main/java/org/jose/jaxrs/api/GroovyScript.java
+++ src/main/java/org/jose/jaxrs/api/GroovyScript.java
@@ -1,16 +1,40 @@
 package org.jose.jaxrs.api;
 
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
 
 public class GroovyScript {
 
-    private String result ;
+  private String script;
+  private String result;
 
-    public String getResult() {
-        return result;
+  private Binding binding;
+
+  public GroovyScript( String script ){
+    this.script = script;
+    binding = new Binding();
+  }
+
+  public String getResult() {
+    return result;
+  }
+
+  public void run() {
+
+    try {
+
+      binding.setVariable("foo", new Integer(2));
+
+      GroovyShell shell = new GroovyShell(binding);
+
+      Object value = shell.evaluate(this.script);
+
+      this.result = value.toString();
+    }
+    catch (Exception err) {
+      this.result = err.toString();
     }
 
-    public void setResult(String result) {
-        this.result = result;
-    }
+  }
 
 }
--- src/main/java/org/jose/jaxrs/server/ScriptService.java
+++ src/main/java/org/jose/jaxrs/server/ScriptService.java
@@ -13,9 +13,23 @@ public class ScriptService {
 	@GET
 	@Path("/s1")
 	public GroovyScript s1() {
-		GroovyScript s1 = new GroovyScript();
-		s1.setResult("OK");
+
+		GroovyScript s1 = new GroovyScript("foo + 10");
+
+		s1.run();
+
 		return s1;
+	}
+
+	@GET
+	@Path("/s2")
+	public GroovyScript s2() {
+
+		GroovyScript s2 = new GroovyScript("foo + 100");
+
+		s2.run();
+
+		return s2;
 	}
 
 }
